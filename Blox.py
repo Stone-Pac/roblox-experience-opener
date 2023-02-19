7e6d2361b38f8e5d7e15895c72b27dfea8ecc5e8
Sogon <107663159+Stone-Pac@users.noreply.github.com>
2023-02-18 21:06:17 -0800

    Update Blox.py

--- Blox.py
+++ Blox.py
@@ -4,7 +4,7 @@ import webbrowser
 universal_id = input("Enter the Universal ID of the Roblox experience: ")
 
 # Construct the URL for the Roblox experience using the Universal ID
-url = f"https://create.roblox.com/creations/experiences/{universal_id}"
+url = f"https://create.roblox.com/creations/experiences/{universal_id}/overview"
 
 # Open the URL in the default web browser
 webbrowser.open(url)
