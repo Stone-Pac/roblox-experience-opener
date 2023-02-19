652376c2b605544d06059cf024a39a0ccf6a6738
Sogon <107663159+Stone-Pac@users.noreply.github.com>
2023-02-18 21:02:13 -0800

    Add files via upload

---
+++ Blox.py
@@ -0,0 +1,10 @@
+import webbrowser
+
+# Prompt the user to input the Universal ID
+universal_id = input("Enter the Universal ID of the Roblox experience: ")
+
+# Construct the URL for the Roblox experience using the Universal ID
+url = f"https://www.roblox.com/games/{universal_id}"
+
+# Open the URL in the default web browser
+webbrowser.open(url)
